9b68f6ae930b6a8f20fb940f47dc93c6812342c0
TheHub1076 <99098241+TheHub1076@users.noreply.github.com>
2022-05-07 17:21:44 +0200

    Add files via upload

---
+++ games/kaijuparadise.lua
@@ -0,0 +1,11 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheHub1076/TheHub/main/dropdown%20ui%20libary.lua", true))()
+
+local tab1 = library:CreateTab("kaiju Paradise", true)
+
+local text = library:MakeLabel(tab1,"The Hub (Script Hub)")
+
+local button = library:MakeButton(tab1,"RealZzHub V2",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RealZzHub/MainV2/main/Main.lua"))()
+end)
+
+local text = library:MakeLabel(tab1,"Jkgamer1_0#1076")
