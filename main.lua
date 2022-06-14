4342f573ec4e801d602ada568df58b8da08d9cb8
TheHub1076 <99098241+TheHub1076@users.noreply.github.com>
2022-06-14 11:40:27 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -14,33 +14,7 @@ local PlayerID = LocalPlayer.UserId
 
 -----------------------------------------------------------------
 
-local HttpService = game:GetService("HttpService");
 
-function SendMessage(Webhook, Message, Botname)
-    if not string.find(Webhook, "https://discordapp.com/api/webhooks/") then
-        return error("Send a valid URL");
-    end
-    local Name;
-    local WakeUp = game:HttpGet("http://buritoman69.glitch.me");
-    local API = "http://buritoman69.glitch.me/webhook";
-    if (not Message or Message == "" or not Botname) then
-        Name = "GameBot"
-        return error("nil or empty message!")
-    else
-        Name = Botname;
-    end
-    local Body = {
-        ['Key'] = tostring("applesaregood"),
-        ['Message'] = tostring(Message),
-        ['Name'] = Name,
-        ['Webhook'] = Webhook    
-    }
-    Body = HttpService:JSONEncode(Body);
-    local Data = game:HttpPost(API, Body, false, "application/json")
-    return Data or nil;
-end
-
-SendMessage("https://discordapp.com/api/webhooks/985898689035894814/psjAdizJLxATso2vFJmZu63Kq1Q3cfgvw9ao9jxUVxcyOPsxKXekUp6L4GyH5PNrZe4q", "**Player Name:** `"..PlayerName.."`   **Player ID:** `"..PlayerID.."`   **Player HWID:** `"..HWID.."`   **Game Name:** `"..GameName.."`   **Game ID:** `"..GameID.."`   **Executor:** `"..Executor.."`", PlayerName)
 
 -----------------------------------------------------------------
 
@@ -793,11 +767,11 @@ _G.loop = true;
 
 while _G.loop == true do
         local Time = DateTime.now()
-        zeit:Set(Time:FormatLocalTime("LT", "en-us"))
-        zeit1:Set(Time:FormatLocalTime("LT", "en-us"))
-        zeit2:Set(Time:FormatLocalTime("LT", "en-us"))
-        zeit3:Set(Time:FormatLocalTime("LT", "en-us"))
-        zeit4:Set(Time:FormatLocalTime("LT", "en-us"))
-        zeit5:Set(Time:FormatLocalTime("LT", "en-us"))
+        zeit:Set("Time: "..Time:FormatLocalTime("LT", "en-us"))
+        zeit1:Set("Time: "..Time:FormatLocalTime("LT", "en-us"))
+        zeit2:Set("Time: "..Time:FormatLocalTime("LT", "en-us"))
+        zeit3:Set("Time: "..Time:FormatLocalTime("LT", "en-us"))
+        zeit4:Set("Time: "..Time:FormatLocalTime("LT", "en-us"))
+        zeit5:Set("Time: "..Time:FormatLocalTime("LT", "en-us"))
         wait(0.1)
 end
