d2964b17cdb2f9ad606dd0f8fc171974b0928e15
TheHub1076 <99098241+TheHub1076@users.noreply.github.com>
2022-06-15 19:35:11 +0200

    Add files via upload

---
+++ games/bloxybingo.lua
@@ -0,0 +1,26 @@
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/TheHub1076/TheHub/main/Wally%20V3.lua')))()
+
+local main = library:CreateWindow("Bloxy Bingo")
+
+local scripts = main:CreateFolder("Scripts")
+
+
+
+-- SCRIPTS
+
+scripts:Button("No Name",function()
+    loadstring(game:HttpGet"https://thedragonslayer2.github.io")()
+end)
+
+
+
+-- END
+
+scripts:Label("",{
+       TextSize = 25;
+       TextColor = Color3.fromRGB(255,255,255);
+       BgColor = Color3.fromRGB(38,38,38);
+       
+})
+
+scripts:DestroyGui()
